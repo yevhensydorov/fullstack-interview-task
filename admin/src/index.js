b4b622a369c30494925423dde9d3f9e71308f10f
Yevhen Sydorov <yevhensydorov@192.168.1.102>
2022-12-18 18:11:02 +0000

    add generate csv endpoint handler

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("config");
 const axios = require("axios");
+const { getCsvReport } = require('./services/getCsvReport');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({ limit: "10mb" }))
+app.use(bodyParser.json({ limit: "10mb" }));
 
 app.get("/investments/:id", async (req, res) => {
   const { id } = req.params;
@@ -21,10 +22,28 @@ app.get("/investments/:id", async (req, res) => {
   }
 });
 
+app.get("/generatecsv", async (_req, res) => {
+  try {
+    const csvReport = await getCsvReport();
+
+    await axios.post(
+      `${config.investmentsServiceUrl}/investments/export`,
+      { data: csvReport },
+      {
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+
+    res.sendStatus(201);
+  } catch (e) {
+    console.log('Error while sendind the report:', e);
+  }
+});
+
 app.listen(config.port, (err) => {
   if (err) {
     console.error("Error occurred starting the server", err)
     process.exit(1)
   }
   console.log(`Server running on port ${config.port}`)
-})
+});
